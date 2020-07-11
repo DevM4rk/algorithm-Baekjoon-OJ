a9285e5100f3557c6b8f0b32bc427c1eb69777a2
YH. Jin <48309387+duqrldudgns@users.noreply.github.com>
2020-07-11 16:52:01 +0900

    Update 9375.cpp

--- 2020_07/9375.cpp
+++ 2020_07/9375.cpp
@@ -23,29 +23,22 @@ using namespace std;
 int main() {
   
   map< string, int > m;
-  map< string, int >::iterator m_iter;
-
+  
   int num,n,sum;
   string name,kind;
 
   cin >> num;
-  for(int i=0;i<num;i++){
+  while(num--){
     cin >> n;
     sum=1;
    
-    for(int j=0;j<n;j++){
+    while(n--){
       cin >> name >> kind;
-
-      auto m_iter = m.find(kind);
-      if( m_iter ==m.end()){
-        m.insert(make_pair(kind, 1));
-      }
-      else{
-       (*m_iter).second++;
-      }
+      m[kind]++;
     }
-    for(auto it = m.begin(); it != m.end(); it++){
-		  sum *= (it->second)+1;
+
+    for(auto a : m){
+		  sum *= a.second+1;
 	  }
     cout << sum-1 << endl;
     m.clear();
@@ -53,7 +46,6 @@ int main() {
 
   return 0;
 }
-
 /*
 aa bbb c
 
