d0e4253ac3bc17cbda262bb8552c0bbd9247e3f1
duqrldudgns <duqrldudgns@gmail.com>
2020-08-06 06:00:34 +0900

    master

---
+++ 2020_08/for&if&random.py
@@ -0,0 +1,12 @@
+from random import *
+
+sum = 0
+for i in range(1,51) :
+    customer = randrange(5,51)
+    if 5 <= customer <= 15 : 
+        check = "O"
+        sum += 1
+    else : 
+        check = " "
+    print("[{0}] {1}번째 손님 (소요시간 : {2}분)".format(check,i,customer))
+print("총 탑승 객 %d" % sum )
