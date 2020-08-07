4254d36b238f16d813a44d7b778fb38c8121cee2
duqrldudgns <duqrldudgns@gmail.com>
2020-08-07 20:32:16 +0900

    master

--- 2020_08/for&if&random.py
+++ 2020_08/for&if&random.py
@@ -1,8 +1,8 @@
-from random import *
+import random
 
 sum = 0
 for i in range(1,51) :
-    customer = randrange(5,51)
+    customer = random.randrange(5,51)
     if 5 <= customer <= 15 : 
         check = "O"
         sum += 1
