39a066b0cfef61c1be63c526866d5d99982d2bd1
duqrldudgns <duqrldudgns@gmail.com>
2020-12-27 21:04:47 +0900

    update

--- 2020_12/1966.cpp
+++ 2020_12/1966.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
 #include <queue>
+#include <string.h>
 
 using namespace std;
 
 queue<int>q;
+queue<int>q2;
 
-int t,N,M,temp,cnt=0;
+int t,N,M,temp,cnt=0,arr[101]={0,}, first, import;
 
 void solve(){
+    for(int i=100; i>=0; i--){
+        if(arr[i]!=0){
+            first = i;
 
+            while( q.front() != first ){
+                temp = q.front();
+                q.pop();
+                q.push(temp);
+
+                temp = q2.front();
+                q2.pop();
+                if( temp == 1) q2.push(1);
+                else q2.push(0);
+            }
+
+            q.pop();
+            cnt++;
+
+            if(q2.front() == 1) break;
+            q2.pop();
+
+            arr[i]--;
+            i++;
+        }
+    }
 }
 
 int main(){
     cin >> t;
+    
     while(t--){
         cin >> N >> M;
-        
+         
         for(int i=0; i<N; i++){
             cin >> temp;
             q.push(temp);
+            arr[temp]++;
+            if( M==0 ) {
+                q2.push(1);
+                M--;
+            }
+            else {
+                q2.push(0);
+                M--;
+            }
         }
 
         solve();
@@ -27,9 +63,8 @@ int main(){
         
         //reset
         q = queue<int>();
+        q2 = queue<int>();
+        memset(arr, 0, 100);
         cnt=0;
     }
 }
-/*
-높은 순으로 맨앞으로 보내서 빼내는 작업해야됨
-*/
