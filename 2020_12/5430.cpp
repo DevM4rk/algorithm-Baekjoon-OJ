6ee976463debdcab675f9245c0d3af8b959b38c3
duqrldudgns <duqrldudgns@gmail.com>
2020-12-30 04:57:52 +0900

    solved

--- 2020_12/5430.cpp
+++ 2020_12/5430.cpp
@@ -3,43 +3,58 @@
 
 using namespace std;
 
-int T,n;
+int T,n,temp;
 string p,arr;
 deque<int> dq;
 bool check = true; //true : R2,  false : R1
 
 void print(){
+    cout << '[';
 
+    if(check){
+        while( true ){
+            temp = dq.front();
+            dq.pop_front();
+            cout << temp ;
+            if ( !dq.empty() ) cout << ',';
+            else break;
+        }
+    }
+
+    else{
+        while( true ){
+            temp = dq.back();
+            dq.pop_back();
+            cout << temp ;
+            if ( !dq.empty() ) cout << ',';
+            else break;
+        }
+    }
+
+    cout << ']' << endl;
 }
-/*
-R일 경우
-
-출력할때도 R의 경우에 따라서 front or back 순서로
-
-D일 경우
-dq 가 비어있을 때 error 발생
-
-R의 경우에 따라서 front or back pop
- 
-
-*/
 
 void solve(){
-    for(int i=0; p[i]>0 ;i++){
+    for(int i=0; p[i]!='\0' ;i++){
         if( p[i] == 'R'){
             check = !check;
         }
         else if( p[i] == 'D'){
+            if( dq.empty() ){
+                cout << "error" << endl;
+                break;
+            }
+
             if(check){
-                
+                dq.pop_front();
             }
             else{
-
+                dq.pop_back();
             }
         }
     }
 
-    print();
+    if ( !dq.empty() ) print();
 }
 
 int main(){
@@ -63,5 +78,89 @@ int main(){
         solve();
 
         dq.clear();
+        check = true;
     }
 }
+
+/*
+#include<iostream>
+#include<string>
+#include<deque>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T, p;
+	bool FR, err;
+	string str, numline; //명령어, 숫자 배열
+
+	cin >> T;
+	int i;
+
+	while (T--) {
+    	deque<int> dq;
+		FR = true;
+		err = false;
+		cin >> str >> p >> numline;
+		i = 1;
+		while (numline[i]!='\0'){
+			int x = 0;
+			while (numline[i] >= '0' and numline[i] <= '9') {
+				x *= 10;
+				x += int(numline[i] - '0');
+				i++;
+			}
+			if (x != 0) {
+				dq.push_back(x);
+			}
+			i++;
+		}
+		
+		i = 0; //변수 또 할당하기 귀찮고 메모리 아깝다고 판단해서 변수 초기화
+		while (str[i]!='\0') { //여기서 문자열 판별
+			if (str[i] == 'R') {
+				FR = !FR;
+			}
+			else if(str[i]=='D') {
+				if (dq.empty()) { //deque가 비어있을때 D명령어가 나오면 error발생
+					cout << "error" << endl;
+					err = true;
+					break;
+				}
+				if (FR) {
+					dq.pop_front();
+				}
+				else {
+					dq.pop_back();
+				}
+			}
+			i++;
+		}
+		
+        //출력하는 부분 error 여부에 따라 '[',']'기호도 출력할지 판단
+		if (!err) {
+			cout << "[";
+		}
+		while (!dq.empty()) { 
+			if (FR) {
+				auto c = dq.front();
+				dq.pop_front();
+				cout << c;
+			}
+			else {
+				auto c = dq.back();
+				dq.pop_back();
+				cout << c;
+			}
+			if (!dq.empty()) {
+				cout << ",";
+			}
+		}
+		if (!err) {
+			cout << "]"<<endl;
+		}
+	}
+}
+*/
