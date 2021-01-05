b9462e9f6fe0db45144842e9a350f257ed512119
엽기영훈 <duqrldudgns@gmail.com>
2021-01-05 23:46:35 +0900

    Update 1003.cpp

--- 2021_01/1003.cpp
+++ 2021_01/1003.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int T, N, arr[41][2]={0,};
 
-void solve(){
+void solve(){ 
     for( int i=2; i<=N; i++){
         arr[i][0] = arr[i-1][0] + arr[i-2][0];
         arr[i][1] = arr[i-1][1] + arr[i-2][1];
