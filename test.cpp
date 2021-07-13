ea9e3fb3720dc9244b0e15fc6119e458b8378e3b
진영훈 <duqrldudgns@gmail.com>
2021-07-13 21:11:02 +0900

    solved

--- test.cpp
+++ test.cpp
@@ -2,11 +2,23 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <stack>
 using namespace std;
 typedef pair<string, int> ii;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef tuple<int,int,int> tp;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector <ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
 int main() {
-    vector<int> v;
+    stack<pii> s;
+    s.top().
     v.push_back(1);
     v.push_back(2);
 
