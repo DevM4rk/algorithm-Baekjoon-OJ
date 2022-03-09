ed7922aea8701e36a232fa6118a4ddd971638906
duqrldudgns <duqrldudgns@gmail.com>
2022-03-09 20:25:04 +0900

    solved

---
+++ 2022_03/13459.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef tuple<int,int,int> tp;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector <ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+struct node{
+    int x1,y1,x2,y2,cnt;
+    node(int x1_, int y1_, int x2_, int y2_, int cnt_) : x1(x1_), y1(y1_), x2(x2_), y2(y2_), cnt(cnt_) {}
+};
+
+int n,m;
+char arr[10][10];
+queue<node> q;
+map<pair<pii,pii>, int> vis;
+
+const int dx[4] = {0,0,1,-1};
+const int dy[4] = {1,-1,0,0};
+
+void move(int &x, int &y, int &cnt, int i){
+    while(arr[x][y] != 'O' && arr[x+dx[i]][y+dy[i]] != '#'){
+        x += dx[i];
+        y += dy[i];
+        cnt++;
+    }
+}
+
+void bfs(){
+    while(!q.empty()){
+        auto cur = q.front(); q.pop();
+
+        cout << cur.x1 << " " << cur.y1 << " " << cur.x2 << " " << cur.y2 << " " << cur.cnt << endl;
+
+        if(cur.cnt >= 10){
+            cout << 0;
+            return;
+        }
+
+        for(int i=0; i<4; ++i){
+            int rx = cur.x1;
+            int ry = cur.y1;
+            int bx = cur.x2;
+            int by = cur.y2;
+            int cnt = cur.cnt+1;
+            int rc=0, bc=0;
+
+            move(rx,ry,rc,i);
+            move(bx,by,bc,i);
+
+            if(arr[bx][by] == 'O') continue;
+            if(arr[rx][ry] == 'O'){
+                cout << 1;
+                return;
+            }
+
+            if(rx == bx && ry == by){
+                if(rc > bc) rx -= dx[i], ry -= dy[i];
+                else bx -= dx[i], by -= dy[i];  
+            }
+
+            if(vis.count({{rx,ry},{bx,by}})) continue;
+            q.push(node(rx,ry,bx,by,cnt));
+            vis[{{rx,ry},{bx,by}}]++;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    int rx,ry,bx,by;
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<m; ++j){
+            cin >> arr[i][j];            
+            if(arr[i][j] == 'R')
+                rx = i, ry = j;
+            else if(arr[i][j] == 'B')
+                bx = i, by = j;
+        }
+    }
+
+    q.push(node(rx,ry,bx,by,0));
+    vis[{{rx,ry},{bx,by}}]++;
+
+    bfs();
+}
--- test.cpp
+++ test.cpp
@@ -1,21 +1,84 @@
-#include <bits/stdc++.h>
-
-#define endl "\n"
-#define all(v) v.begin(), v.end()
+#include <iostream>
+#include <queue>
 
 using namespace std;
 
-typedef long long ll;
-typedef tuple<int,int,int> tp;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector <ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
+struct bead {
+    int rx, ry, bx, by, d;
+};
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
+int m = 0, n = 0;
+char map[10][10];
+bool check[10][10][10][10];
+queue<bead> q;
+const int dx[] = {-1, 0, 1, 0}, dy[] = {0, -1, 0, 1};
 
+void move(int &x, int &y, int &c, int dx, int dy) {
+    while (map[x][y] != 'O' && map[x+dx][y+dy] != '#') {
+        x += dx;
+        y += dy;
+        c++;
+    }
+}
+
+void bfs() {
+    while (!q.empty()) {
+        int rx = q.front().rx;
+        int ry = q.front().ry;
+        int bx = q.front().bx;
+        int by = q.front().by;
+        int d = q.front().d;
+        
+        cout << rx << " " << ry << " " << bx << " " << by << " " << d << endl;
+
+        q.pop();
+        
+        if (d >= 10) break;
+        
+        for (int i = 0; i < 4; i++) {
+            int nrx = rx, nry = ry, nbx = bx, nby = by, nd = d + 1;
+            int rc = 0, bc = 0;
+            
+            move(nrx, nry, rc, dx[i], dy[i]);
+            move(nbx, nby, bc, dx[i], dy[i]);
+            
+            if (map[nbx][nby] == 'O') continue;
+            if (map[nrx][nry] == 'O') {
+                printf("1\n");
+                return;
+            }
+            
+            if (nrx == nbx && nry == nby) {
+                if (rc > bc) nrx -= dx[i], nry -= dy[i];
+                else nbx -= dx[i], nby -= dy[i];
+            }
+            
+            if (check[nrx][nry][nbx][nby]) continue;
+            check[nrx][nry][nbx][nby] = true;
+            q.push({nrx, nry, nbx, nby, nd});
+        }
+    }
+    
+    printf("0\n");
+}
+
+int main() {
+    int rx = 0, ry = 0, bx = 0, by = 0;
+    
+    scanf("%d %d", &n, &m);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%1s", &map[i][j]);
+            
+            if (map[i][j] == 'R') rx = i, ry = j;
+            else if (map[i][j] == 'B') bx = i, by = j;
+        }
+    }
+    
+    check[rx][ry][bx][by] = true;
+    q.push({rx, ry, bx, by, 0});
+    bfs();
+    
+    return 0;
 }
