08320ff121374ace74a04d8597cfb05ab18e6a91
duqrldudgns <duqrldudgns@gmail.com>
2022-03-23 18:15:50 +0900

    test

--- test.cpp
+++ test.cpp
@@ -18,42 +18,4 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n,t; cin >> n >> t;
-
-    vi limit(n);
-    for(int i=0; i<n-1; ++i){
-        int size; cin >> size;
-        limit[i] = size;
-    }
-    limit[n-1] = INT_MAX; // 2e9
-    
-    vpii v[n];
-
-    while(t--){
-        char c; cin >> c;
-        if(c == 'I'){
-            int k,s; cin >> k >> s;
-            
-            for(int i=0; i<v[0].size(); i++){
-                if(v[0][i].first == k){
-                    v[0].erase(v[0].begin()+i);
-                    break;
-                }
-            }
-            v[0].push_back({k,s});
-        }
-        else{ // F
-            int k; cin >> k;
-            //cout 
-        }
-    }
 }
-/*
-먼저 설계부터
-insert 연산 시
-레벨 1의 같은 키 있는지 확인 후 기존에 있던거 삭제, 새로운 메모리 할당
-위 연산ㅇ ㅣ끝나고 난 후
-메모리 제한을 넘기게 될 경우, avalanche 연산이 이루어 져야함
-
-
-*/
