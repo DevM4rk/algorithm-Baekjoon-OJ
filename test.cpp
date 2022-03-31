33915e06bca7423bd031d596b9c30e3e036ace9d
duqrldudgns <duqrldudgns@gmail.com>
2022-04-01 04:52:49 +0900

    solved

--- test.cpp
+++ test.cpp
@@ -1,31 +1,25 @@
-#include <bits/stdc++.h>
-
-#define endl "\n"
-#define all(v) v.begin(), v.end()
-
+#include<iostream>
+#include<string>
+ 
 using namespace std;
-
-typedef long long ll;
-typedef tuple<int,int,int> tp;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector <ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-
-    int a = 10;
-    int *ptr = &a;
-    int &ref = a;
-
-    *ptr = 20;
-    cout << *ptr << ref;
+ 
+template <typename T>
+T sum(T a, T b){
+    return a + b;
+}
+ 
+int main(void) {
+    int a=1, b =2;
     
-    ref = 30;
-    cout << *ptr << ref;
+    double d1 = 2.2;
+    double d2 = 3.3;
+ 
+    string s1 = "Show me ";
+    string s2 = "The Money 6";
     
+    cout << "int 합 : " << sum<int>(a, b) << endl;
+    cout << "double 합 : " << sum<double>(d1, d2) << endl;
+    cout << "string 합 : " << sum<string>(s1, s2) << endl;
+    
+    return 0;
 }
