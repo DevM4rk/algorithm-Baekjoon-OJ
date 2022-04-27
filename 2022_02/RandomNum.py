de74a2be237ed0fdd76aced6f173576f119e9b88
duqrldudgns <duqrldudgns@gmail.com>
2022-04-27 20:15:50 +0900

    update

--- 2022_02/RandomNum.py
+++ 2022_02/RandomNum.py
@@ -1,6 +1,25 @@
 import random
 
-print("Print RandomNum 97 ~ 109.9")
+print("Print RandomNum Program")
+
+while 1 :
+    print("\n(If you want to print from 10, enter 10)")
+    print("Set minimum number : ", end = '')
+    MIN = float(input())
+
+    print("\n(If you want to print up to 19.9, enter 20)")
+    print("Set maximum number : ", end = '')
+    MAX = float(input())
+    MAX = MAX - 0.1
+
+    if MIN < MAX :
+        break
+    else :
+        print("\nError \n- {} ~ {} is impossible.".format(MIN,MAX))
+        print("- Please enter the minimum number less than the maximum number.")
+
+print("\nSetting complete!!")
+print("Print RandomNum {} ~ {}".format(MIN,MAX))
 print("Enter key to continue...")
 
 idx=0
@@ -9,6 +28,6 @@ while 1 :
     idx+=1
     print("--",idx)
     for i in range(8) :
-        a = random.uniform(97, 109.9)
+        a = random.uniform(MIN, MAX)
         print("{:.1f}".format(a).ljust(5), end='  ')
     print()
