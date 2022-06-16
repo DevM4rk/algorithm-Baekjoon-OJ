920f67b2d4585c3c986af0582adf49ba029158c6
duqrldudgns <duqrldudgns@gmail.com>
2022-06-16 21:38:33 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,25 +1,21 @@
-#include<iostream>
-#include<string>
- 
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+
 using namespace std;
- 
-template <typename T>
-T sum(T a, T b){
-    return a + b;
-}
- 
-int main(void) {
-    int a=1, b =2;
-    
-    double d1 = 2.2;
-    double d2 = 3.3;
- 
-    string s1 = "Show me ";
-    string s2 = "The Money 6";
-    
-    cout << "int 합 : " << sum<int>(a, b) << endl;
-    cout << "double 합 : " << sum<double>(d1, d2) << endl;
-    cout << "string 합 : " << sum<string>(s1, s2) << endl;
-    
-    return 0;
+
+typedef long long ll;
+typedef tuple<int,int,int> tp;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector <ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
 }
