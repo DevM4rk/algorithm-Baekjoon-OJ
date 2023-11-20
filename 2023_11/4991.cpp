38dd55fe7a1e43d86b65f650ef2f020cc03064be
duqrldudgns <duqrldudgns@gmail.com>
2023-11-20 18:10:57 +0900

    ing

---
+++ 2023_11/4991.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m,ans;
+int board[20][20];
+
+int main(){
+	
+	while(true){
+		cin >> m >> n;
+		if(n==0 && m==0) break;
+
+		//초기화 하기
+		ans=-1;
+
+
+
+	}
+}
+/*
+2의 10승
+0000000000
+
+반례
+11 11
+....x.x....
+....x.x....
+....x*x....
+....x*x....
+xxxxx.xxxxx
+****.o.***.
+xxxxx.xxxxx
+....x*x....
+....x.x....
+....x.x....
+....x.x....
+0 0
+답 23
+*/
