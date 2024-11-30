a7d911e10da986d76704c08526926d368c8d96fb
진영훈 <duqrldudgns@gmail.com>
2024-11-30 22:26:32 +0900

    solved

---
+++ 2024_11/가희1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef tuple<int,int,int> tp;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector <ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int a,b; cin >> a >> b;
+    if( a < b)
+    cout << "high speed rail";
+    else
+    cout << "flight";
+}
