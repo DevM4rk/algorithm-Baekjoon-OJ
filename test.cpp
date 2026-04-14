614c1dd9bc7c794a92f5883105b06e12f9d030ad
duqrldudgns <duqrldudgns@gmail.com>
2026-04-14 19:53:30 +0900

    solved

--- test.cpp
+++ test.cpp
@@ -17,10 +17,14 @@ typedef vector<pll> vpll;
 int main(){
     // ios_base::sync_with_stdio(false);
     // cin.tie(nullptr); cout.tie(nullptr);
+    std::string a = "Hello World";
+string b;
 
-    for(int i=0; i<3; i++){
-    string s;
-    getline(cin, s);
-    cout << s;
-    }
+    std::cout << "a: \"" << &a << "\"\n"; // a는 비어있음
+    std::cout << "b: \"" << &b << "\"\n"; // b가 원래 값 가져감
+
+    b = std::move(a); // 이동
+
+    std::cout << "a: \"" << &a << "\"\n"; // a는 비어있음
+    std::cout << "b: \"" << &b << "\"\n"; // b가 원래 값 가져감
 }
